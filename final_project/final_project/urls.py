aa20ccb3707b6e045c79918ea0096c6f59df1a4b
Kostya Golunov <kostya.golunov2015@yandex.ru>
2023-01-03 21:30:34 +0500

    Add image for main page

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from mainApp.views import index_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_page),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- final_project/mainApp/models.py
+++ final_project/mainApp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Profession(models.Model):
     title = models.CharField('Название', max_length=50)
     description = models.TextField('Описание')
-    # image = models.ImageField('Картинка')
+    image = models.ImageField(upload_to='./static/image')
 
     def __str__(self):
         return self.title
